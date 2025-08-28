830b674aa0af3fc4dc2617204b9ce76680415c7e
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2025-08-28 07:24:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = os.environ.get("API_ID", "26872474")
+API_HASH = os.environ.get("API_HASH", "f8d3a289bf28a13a7159ad0b2ed114e7")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 ADMIN = int(os.environ.get("ADMIN", ""))
 PICS = (os.environ.get("PICS", "")).split()
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002057608700"))
 
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split())) # Add Multiple channel ids
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1001590803749").split())) # Add Multiple channel ids
